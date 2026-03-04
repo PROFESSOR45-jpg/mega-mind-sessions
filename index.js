ba0e8c845a952f05315c7b0ba89af37427ca8812
MARK WAMBIA <wambiamark45@gmail.com>
2026-03-04 11:15:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+/**
+ * MEGA MIND SESSIONS - Main Export
+ * Advanced authentication for WhatsApp Baileys
+ */
 
+const MultiFileAuthState = require('./lib/MultiFileAuthState');
+const { useEnhancedMultiFileAuthState, SessionManager } = require('./lib/enhancedAuth');
+
+/**
+ * Standard multi-file auth (Baileys compatible)
+ */
+async function useMultiFileAuthState(folderPath = './session') {
+    const auth = new MultiFileAuthState(folderPath);
+    return await auth.init();
+}
+
+/**
+ * Enhanced auth with export/import
+ */
+async function useMegaMindAuth(folderPath = './session') {
+    return await useEnhancedMultiFileAuthState(folderPath);
+}
+
+module.exports = {
+    // Core classes
+    MultiFileAuthState,
+    SessionManager,
+    
+    // Functions
+    useMultiFileAuthState,
+    useMegaMindAuth,
+    useEnhancedMultiFileAuthState
+};
+      
